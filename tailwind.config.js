102c444ee70a645ecdf7e3989774b03375b47bd2
Michael Godfrey <m.godfrey59@gmail.com>
2024-12-23 23:47:35 +1000

    added new photos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero': "url('/images/ech-hero.jpg')",
+      }
+    },
   },
   plugins: [],
 }
